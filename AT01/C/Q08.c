fb556cc55e249490a5fe592f47319d6324e4d83b
Mayra_Oliveira <mayracaetano096@gmail.com>
2022-02-12 16:19:45 -0300

    Questão 08 corrigida em C

--- AT01/C/Q08.c
+++ AT01/C/Q08.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int lado, count=1;
+    int lado1, lado2, count=1;
 
-    printf("Lado do quadrado: ");
-    scanf("%d", &lado);
+    scanf("%d", &lado1);
+    scanf("%d", &lado2);
 
-    while( count <= lado*lado){
-        if(count % lado == 0){
+    while( count <= lado1*lado2){
+        if(count % lado2 == 0){
             printf("*\n");
         }else{
             printf("*");
