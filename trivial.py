f32cdedecc248ac1c9bd289f5784e0724f2e9438
Marcos Dalessandro <mdclima12@gmail.com>
2022-02-12 05:19:36 -0300

    Atividade_01_py

---
+++ trivial.py
@@ -0,0 +1,13 @@
+x=int(input())
+y=int(input())
+mdc=1
+def mdc():
+    if x>y:
+        maior=x
+    else:
+        maior=y
+    for i in range(1,maior):
+        if x%i==0 and y%i ==0:
+            mdc=i
+    print (mdc)
+mdc()
