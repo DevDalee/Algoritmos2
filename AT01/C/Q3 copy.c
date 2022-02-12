4e7451d8924ae4c1174307128583cb9be38fc7c3
Mayra_Oliveira <mayracaetano096@gmail.com>
2022-02-12 16:55:55 -0300

    Questão 03 em C

---
+++ AT01/C/Q3 copy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    char y, x;
+
+    scanf("%c\n", &y);
+    scanf("%c", &x);
+
+
+    if ( VOLTA == “S” ) | ( VOLTA == “s” )
+ if ( OPCAO == 1 )
+ PRECO = 900;
+ elseif ( OPCAO == 2 )
+ PRECO = 650;
+ elseif ( OPCAO == 3 )
+ PRECO = 600;
+ elseif ( OPCAO == 4 )
+ PRECO = 550;
+ else
+ disp(“Opcao invalida!”);
+ end
+else
+ if ( OPCAO == 1 )
+ PRECO = 500;
+ elseif ( OPCAO == 2 )
+ PRECO = 350;
+ elseif ( OPCAO == 3 )
+ PRECO = 350;
+ elseif ( OPCAO == 4 )
+ PRECO = 300;
+ else
+ disp(“Opcao invalida!”);
+ end
+end
+fprintf(1,”Preco: %.2f\n”,PRECO);
+}
