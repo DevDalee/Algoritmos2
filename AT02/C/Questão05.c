1a5ff545f5e7c71a9be86c47bd33ff498ff1054d
marcos-dalessandro <mdclima12@gmail.com>
2022-02-22 16:56:30 -0300

    Atividade Completa

--- AT02/C/Questão05.c
+++ AT02/C/Questão05.c
@@ -6,10 +6,8 @@ int main(){
 
     char nome[L][C];
     float medidas[5][3];
-    int k,i, j;
-    float media_alt = 0, media_peso = 0, media_ira = 0;
-    float maior_alt = 0, maior_peso =0, maior_ira=0;
-    float menor_alt=0, menor_peso=0, maior_ira=0;
+    int k,i,j;
+    float media_alt = 0, media_peso = 0, media_ira = 0, maior_alt = 0, maior_peso = 0, maior_ira = 0, menor_alt = 0, menor_peso = 0, menor_ira = 0;
 
     printf("Nomes: ");
     for(k = 0; k < L; k++){  
@@ -22,41 +20,72 @@ int main(){
         }
     }
     printf("Media das alturas:");
-    for (i = 0; i < 1; i++){
-        for(j = 0; j <= 4; j++){
-            media_alt += medidas[i][j];
-            media_alt = media_alt / 5; 
+    for (i = 0; i <= 4; i++){
+        for(j = 0; j < 1; j++){
+            media_alt += medidas[i][j]; 
             if(medidas[i][j] > maior_alt){
                 maior_alt = medidas[i][j];
             }
+            menor_alt = medidas[i][j];
         }
     }
-    printf("%.2f\n", media_alt);
-    printf("Maior altura: %f", maior_alt);
+    media_alt /= 5;
+    for ( i = 0; i <= 4; i++){
+        for (j = 0; j < 1; j++){
+            if(medidas[i][j] < menor_alt){
+                menor_alt = medidas[i][j];
+            }
+        }
+        
+    }
+    
+    printf(" %.2f\n", media_alt);
+    printf("Maior altura: %.2f\n", maior_alt);
+    printf("Menor altura: %.2f\n", menor_alt);
     
     printf("Media dos pesos:");
-    for (i = 1; i < 2; i++){
-        for(j = 0; j <= 4; j++){
-            media_alt += medidas[i][j];
-            media_alt = media_alt / 5; 
+    for (i = 0; i <= 4; i++){
+        for(j = 1; j < 2; j++){
+            media_peso += medidas[i][j];            
             if(medidas[i][j] > maior_peso){
                 maior_peso = medidas[i][j];
             }
+            menor_peso = medidas[i][j];
         }
     }
-    printf("%.2f\n", media_peso);
-    printf("Maior peso: %f", maior_peso);
+    media_peso /= 5;
+    for ( i = 0; i <= 4; i++){
+        for (j = 1; j < 2; j++){
+            if(medidas[i][j] < menor_peso){
+                menor_peso = medidas[i][j];
+            }
+        }
+        
+    }
+    printf(" %.2f\n", media_peso);
+    printf("Maior peso: %.2f\n", maior_peso);
+    printf("Menor peso: %.2f\n", menor_peso);
 
     printf("Media dos IRAs:");
-    for (i = 2; i < 3; i++){
-        for(j = 0; j <= 4; j++){
-            media_alt += medidas[i][j];
-            media_alt = media_alt / 5; 
+    for (i = 0; i <= 4; i++){
+        for(j = 2; j < 3; j++){
+            media_ira += medidas[i][j];            
             if(medidas[i][j] > maior_ira){
                 maior_ira = medidas[i][j];
             }
+            menor_ira = medidas[i][j];
         }
     }
-    printf("%.2f", media_ira);
-    printf("Maior IRA: %f", maior_ira);
+    media_ira /= 5;
+    for ( i = 0; i <= 4; i++){
+        for (j = 2; j < 3; j++){
+            if(medidas[i][j] < menor_ira){
+                menor_ira = medidas[i][j];
+            }
+        }
+        
+    }
+    printf(" %.2f\n", media_ira);
+    printf("Maior IRA: %.2f\n", maior_ira);
+    printf("Menor IRA: %.2f\n", menor_ira);
 }
