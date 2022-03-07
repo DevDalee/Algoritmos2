3097e2c221f0265e0254be3768250d3b79fe1bfc
marcos-dalessandro <mdclima12@gmail.com>
2022-03-07 14:56:25 -0300

    Questão 01 - Completa em C, Questão 03 em andamento

--- AT04/C/questao01.c
+++ AT04/C/questao01.c
@@ -8,7 +8,7 @@ struct aluno{
 
 void main(){
     struct aluno c[n];
-    char maior1[15], maior2[15], maior3[15];
+    int end1, end2, end3;
     float Mpeso, Maltura, Mira, maxPeso = 0, maxAlt = 0, maxIra = 0;
     for(int i=0; i<n; i++){
         scanf("%s %f %f %f", c[i].nome, &c[i].altura, &c[i].peso, &c[i].mediaIra);
@@ -18,20 +18,20 @@ void main(){
 
         if (maxPeso < c[i].peso){
             maxPeso = c[i].peso;
-            maior1[15] = c[i].nome;
+            end1 = i;
         }if (maxAlt < c[i].altura){
             maxAlt = c[i].altura;
-            maior2[15] = c[i].nome;
+            end2 = i;
         }if (maxIra < c[i].mediaIra){
             maxIra = c[i].mediaIra;
-            maior3[15] = c[i].nome;
+            end3 = i;
         }
     }
     	
-    printf("Media de Peso: %.3f\n", Mpeso/=5);
+    printf("\nMedia de Peso: %.3f\n", Mpeso/=5);
     printf("Media de Altura: %.3f\n", Maltura/=5);
     printf("Media de IRA: %.3f\n", Mira/=5);
-    printf("Maior Peso: %s  %.3f\n", maior1, maxPeso);
-    printf("Maior Altura: %s  %.3f\n", maior2, maxAlt);
-    printf("Maior IRA: %s  %.3f\n", maior3, maxIra);
+    printf("Maior Peso: %s  %.3f\n", c[end1].nome, maxPeso);
+    printf("Maior Altura: %s  %.3f\n",c[end2].nome , maxAlt);
+    printf("Maior IRA: %s  %.3f\n", c[end3].nome, maxIra);
 }
--- AT04/C/questao03.c
+++ AT04/C/questao03.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 #define n_card 14
-    char *numeros[n_card] = {'Às', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Valete', 'Damas', 'Rei'};
-    char *naipes[4] = {'Paus', 'Ouros', 'Copas', 'Espadas'};
+    char *numeros[n_card] = {"As", "Um", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete", "Damas", "Rei"};
+    char *naipes[4] = {"Paus", "Ouros", "Copas", "Espadas"};
 
 int main(){   
-    srand(time(0));
     int d,i;
     scanf("%d", &d);
-
+    
     if (d == 1){
-        for (i = 0; i < 15 ; i++){
-            printf("%s de %s", numeros[i], naipes[0]);
+        for (i = 0; i < 14 ; i++){
+            printf("%s de %s\n", numeros[i], naipes[0]);
         }
-        for (i = 0; i < 15 ; i++){
-            printf("%s de %s", numeros[i], naipes[1]);
+        for (i = 0; i < 14 ; i++){
+            printf("%s de %s\n", numeros[i], naipes[1]);
         }
-        for (i = 0; i < 15 ; i++){
-            printf("%s de %s", numeros[i], naipes[2]);
+        for (i = 0; i < 14 ; i++){
+            printf("%s de %s\n", numeros[i], naipes[2]);
         }
-        for (i = 0; i < 15 ; i++){
-            printf("%s de %s", numeros[i], naipes[3]);
+        for (i = 0; i < 14 ; i++){
+            printf("%s de %s\n", numeros[i], naipes[3]);
         }
     }
-    
-    
+      
 }
