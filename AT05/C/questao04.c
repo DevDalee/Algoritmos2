577b2e6541e07105aed20ccd225ae633f4b504b2
marcos-dalessandro <mdclima12@gmail.com>
2022-03-14 16:44:48 -0300

    Questão 04 e 05 em andamento

--- AT05/C/questao04.c
+++ AT05/C/questao04.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-float rz1, iz1, rz2, iz2, rz3 = 0, iz3 = 0;
+float rz1, iz1, rz2, iz2, rz3, iz3;
 
 int main(){
    scanf("%f %f %f %f", &rz1, &iz1, &rz2, &iz2);
-   somaC(&rz1, &iz1, &rz2, &iz2);
+   somaCom(&rz1, &iz1, &rz2, &iz2, &rz3, &iz3);
    printf("%.1f + %.1fi\n", rz3, iz3);
 }
-void somaC( float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3){
+void somaCom( float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3 ){
    *rz3 = rz1 + rz2;
    *iz3 = iz1 + iz2;
 }
