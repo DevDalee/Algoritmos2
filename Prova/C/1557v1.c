0946634f864ef4f6f43569c29dead84c84fb53a3
marcos-dalessandro <mdclima12@gmail.com>
2022-03-23 17:00:13 -0300

    Prova Concluida

--- Prova/C/1557v1.c
+++ Prova/C/1557v1.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 int n,T,t,p[31],d[31];
-int preencher(){
+int main(){
     p[0] = 1;
-    for(int i=1; i<31; i++) p[i] = p[i-1] << 1{};
-    for(int i=0; i<31; i++) d[i] = (int) ceil(log10(p[i]+1)){};
-        printf("n");
-}
-int imprimir(){
-    while(scanf("%d",&n) && n){
+    for(int i=1; i<31; i++) p[i] = p[i-1] << 1;
+    for(int i=0; i<31; i++) d[i] = (int) ceil(log10(p[i]+1));
+    while(scanf("%d",&n) and n){
         T = d[2*n-2];
         for(int i=0; i<n; i++){
             for(int j=0; j<n-1; j++){
@@ -19,10 +16,7 @@ int imprimir(){
             while(t--) printf(" ");
             printf("%dn",p[i+n-1]);
         }
+        printf("n");
     }
-}
-int main(){
-    preencher();
-    imprimir();
     return 0;
 }
