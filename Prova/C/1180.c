9115d89d65c5ad9400c21755338bbca0ae5446c7
marcos-dalessandro <mdclima12@gmail.com>
2022-03-30 14:00:26 -0300

    Atividade 07 - Completa

--- Prova/C/1180.c
+++ Prova/C/1180.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
+struct Menor{
+    int tamanho;
+    int posicao;
+};
+
 int main(){
-   int a,b,c,d,e,n, p, men;
-    scanf("%d", &n);
-    int x[n];
-    for(a=0; a<n; a++)
-        scanf("%d", &x[a]);
-    men=x[0];
-    for(a=1; a<n; a++)
-    {
-        if(men>x[a])
-        {
-            men=x[a];
-            p=a;
+    struct Menor *v;
+    int i, tam, men, pos;
+
+    scanf("%d", &tam);
+    int V[tam];
+
+    for(i = 0; i < tam; i++)
+        scanf("%d", &V[i]);
+    men = V[0];
+
+    for(i = 1; i < tam; i++){
+        if(men > V[i]){
+            men = V[i];
+            pos = i;
         }
     }
-    printf("Menor valor: %d\nPosicao: %d\n", men, p);
+    printf("Menor valor: %d\nPosicao: %d\n", men, pos);
     return 0;
 }
